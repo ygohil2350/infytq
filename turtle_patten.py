0f9028a41a962f704553b7dffdf9def31b68c6a9
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-11 11:42:52 +0530

    Create turtle_patten.py

---
+++ turtle_patten.py
@@ -0,0 +1,24 @@
+import turtle
+wn = turtle.Screen()        # creates a graphics window
+wn.setup(540,508)           # set window dimension
+
+alex = turtle.Turtle()      # create a turtle named alex
+alex.shape("turtle")        # alex looks like a turtle
+alex.color('red')           # alex has a color
+
+
+alex.circle(50)
+alex.forward(100)
+alex.backward(200)               
+alex.left(90)     
+alex.forward(100)     
+alex.right(90)  
+alex.forward(200)        
+alex.right(90)  
+alex.forward(100)   
+               
+
+
+#Write the logic to draw the given pattern
+#Refer the statements given above to draw the pattern
+                        
