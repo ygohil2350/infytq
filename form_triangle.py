1dd40d109fbc0229d81d0f1f1d19eee0bd964950
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-11 11:53:17 +0530

    Create form_triangle.py

---
+++ form_triangle.py
@@ -0,0 +1,18 @@
+#lex_auth_012693802383794176153
+
+def form_triangle(num1,num2,num3):
+    #Do not change the messages provided below
+    success="Triangle can be formed"
+    failure="Triangle can't be formed"
+
+    #Write your logic here
+
+    #Use the following messages to return the result wherever necessary
+    return success
+    return failure
+
+#Provide different values for the variables, num1, num2, num3 and test your program
+num1=3
+num2=3
+num3=5
+form_triangle(num1, num2, num3)
