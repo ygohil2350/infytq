286fbfd1e400af12fa0cd201987b7c3286c1d104
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-11 11:55:14 +0530

    Update make_amount.py

--- make_amount.py
+++ make_amount.py
@@ -5,6 +5,22 @@ def make_amount(rupees_to_make,no_of_five,no_of_one):
     one_needed=0
 
     #Start writing your code here
+    five_needed = int(rupees_to_make/5)
+    one_needed = rupees_to_make%5
+    if five_needed <= no_of_five and one_needed <= no_of_one:
+        print("No. of Five needed :", five_needed)
+        print("No. of One needed  :", one_needed)    
+    elif five_needed > no_of_five:
+        total = no_of_five*5
+        one_needed = rupees_to_make - total
+        if one_needed <= no_of_one:
+            print("No. of Five needed :", no_of_five)
+            print("No. of One needed  :", one_needed)  
+        else:
+            print(-1)
+    else:
+        print(-1)
+     
     #Populate the variables: five_needed and one_needed
 
 
