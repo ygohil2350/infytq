6b1341d36f23dc74e47141b3d275f73712e9561c
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-11 11:53:52 +0530

    Create make_amount.py

---
+++ make_amount.py
@@ -0,0 +1,20 @@
+#lex_auth_012693780491968512136
+
+def make_amount(rupees_to_make,no_of_five,no_of_one):
+    five_needed=0
+    one_needed=0
+
+    #Start writing your code here
+    #Populate the variables: five_needed and one_needed
+
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+
+    #print("No. of Five needed :", five_needed)
+    #print("No. of One needed  :", one_needed)
+    #print(-1)
+
+
+#Provide different values for rupees_to_make,no_of_five,no_of_one and test your program
+make_amount(28,8,5)
