6a04a7f87dd4d8f4b452dcd19701a3eaa312f69f
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-12 13:55:38 +0530

    Update find_strong_numbers.py

--- find_strong_numbers.py
+++ find_strong_numbers.py
@@ -1,38 +1,28 @@
-#lex_auth_01269437118923571233
 strong_num_list =[]
 def factorial(number):
-    fact=1
-    if(number == 0 or number == 1):
-        fact = 1
-        
-    else: 
-        for i in range(1,number + 1):
-            fact = fact*i
-            
-             
+    if number == 1:
+        return (number)
+    elif number == 0:
+        return 1
+    else:
+        return (number * factorial(number-1))
+
     return fact
     #remove pass and write your logic to find and return the factorial of given number
 
 
 def find_strong_numbers(num_list):
     #remove pass and write your logic to find and return the list of strong numbers from the given list
-     
-    number=0
-    for z in num_list: 
-        temp = z
-        sum = 0
-        while(temp): 
-            number = temp % 10
-            sum =sum + factorial(number) 
-            temp = temp // 10
-        if(sum == z): 
-            strong_num_list.append(z) 
-        else: 
-            pass  
-              
-    return strong_num_list 
-
-
-num_list=[145,375,100,2,10]
+    result = []
+    for num in num_list:
+        temp = str(num)
+        answer = 0
+        for t in temp:
+            answer = answer + factorial(int(t))
+        if num == answer:
+            result.append(num)
+    return (result)
+ 
+num_list=[145,375,100,2,10,40585,0]
 strong_num_list=find_strong_numbers(num_list)
 print(strong_num_list)
