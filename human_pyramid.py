a437fdd06b4329bca7c1fba204058128f01343da
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-13 20:45:36 +0530

    Create human_pyramid.py

---
+++ human_pyramid.py
@@ -0,0 +1,23 @@
+#lex_auth_01269437527007232044
+
+def human_pyramid(no_of_people):
+    if (no_of_people==1):
+        return 1*50
+    else:
+        return no_of_people*50+human_pyramid(no_of_people-2)
+
+def find_maximum_people(max_weight):
+    no_of_people=1
+    while True:
+        weight = human_pyramid(no_of_people)    
+        
+        if weight <= max_weight:
+            no_of_people += 2
+        else:
+            break
+        
+    return no_of_people-2
+
+#Provide different values for max_weight and test your program
+max_people=find_maximum_people(1000)
+print(max_people)
