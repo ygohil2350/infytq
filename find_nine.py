dc554e9c8cb4907a885f627d50f8e52d7c93f662
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-19 12:53:37 +0530

    Create find_nine.py

---
+++ find_nine.py
@@ -0,0 +1,9 @@
+#lex_auth_0127135811649044481146
+
+def find_nine(nums):
+    #Remove pass and write your code here
+	return 9 in nums and nums.index(9)<4
+    
+
+nums=[1,9,4,5,6]
+print(find_nine(nums))
