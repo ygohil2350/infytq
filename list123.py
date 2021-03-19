69a571b5c3006e1d0a8b623df7bf5dc7bd5262c9
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-19 13:07:04 +0530

    Create list123.py

---
+++ list123.py
@@ -0,0 +1,10 @@
+#lex_auth_0127135869481369601150
+
+def list123(nums):
+    #start writing your code here
+    return '123' in ''.join(map(str,nums))
+
+    
+
+nums=[1,2,3,4,5]
+print(list123(nums))
