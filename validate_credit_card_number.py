abc913a67b29bb756340973e9254542892d74664
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-19 12:19:49 +0530

    Create validate_credit_card_number.py

---
+++ validate_credit_card_number.py
@@ -0,0 +1,32 @@
+#lex_auth_01269445968039936095
+
+def validate_credit_card_number(card_number):
+    #start writing your code here
+    card=str(card_number)
+    l=[]
+    if len(card)==16:
+        for i in card[-2::-2]:
+            n=int(i)*2
+            if n>9:
+                n1=n%10+n//10
+                l.append(n1)
+            else:
+                l.append(n)
+        return (sum(l)+sum(list(map(int,card[-1::-2]))))%10==0
+    else:
+        return False
+
+card_number= 5239512608615007
+result=validate_credit_card_number(card_number)
+if(result):
+    print("credit card number is valid")
+else:
+    print("credit card number is invalid")
+    
+
+card_number= 1456734512345698 #4539869650133101  #1456734512345698 # #5239512608615007
+result=validate_credit_card_number(card_number)
+if(result):
+    print("credit card number is valid")
+else:
+    print("credit card number is invalid")
