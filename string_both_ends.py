82d69aecf479ea62139095f74cea56ed6f07989a
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-19 13:31:27 +0530

    Create string_both_ends.py

---
+++ string_both_ends.py
@@ -0,0 +1,10 @@
+#lex_auth_0127135945621340161163
+
+def string_both_ends(input_string):
+	#start writing your code here
+    if len(input_string)>1:
+        return input_string[0:2]+input_string[(len(input_string)-2)::]
+    else:return -1
+
+input_string="se"
+print(string_both_ends(input_string))
